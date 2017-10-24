27d990ad8bf549a4a6e8725f2fba69728f7295f7
Ritvik Durgempudi <durgempudirr@s.dcsdk12.org>
2017-10-24 08:25:40 -0600

    Introduce accumulation pattern

--- iteration.py
+++ iteration.py
@@ -18,13 +18,35 @@ def add_one(list):
 	for i in range(0, len(list)):
 		list[i] += 1
 	return list
+
 def print_scores(names, scores):
 	for i in range(0, len(names)):
 		print names[i] , " scored " , scores[i]
 
 
 # filter pattern
+# exclude a calculation from list members
+
 def congratulations(names, scores):
 	for i in range(0, len(names)):
 		if (scores[i] == 100):
 			print "Congrats", names[i], "! You got a perfect score!"
+
+
+# accumulation pattern - a type of iteration
+# keep track of other data as we go
+
+def sum(numbers):
+	total = 0
+	for n in numbers:
+		total += n
+	
+	return total
+
+def max(numbers):
+	current_max = numbers[0]
+	for n in numbers:
+		if n > current_max:
+			current_max = n
+
+	return current_max
